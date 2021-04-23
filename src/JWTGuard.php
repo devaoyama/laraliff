006a18b7ba308fbe74b12b1a88144b0b8efe0d9c
devkeita <keita.mywork@gmail.com>
2021-04-23 22:26:46 +0900

    update: attempt update param delete

--- src/JWTGuard.php
+++ src/JWTGuard.php
@@ -6,16 +6,14 @@ use Devkeita\Laraliff\Services\LiffVerificationService;
 
 class JWTGuard extends \Tymon\JWTAuth\JWTGuard
 {
-    public function attempt(array $credentials = [], $login = true, $update = false)
+    public function attempt(array $credentials = [], $login = true)
     {
         $verificationService = new LiffVerificationService(); // Todo DIを使う
         $liff = $verificationService->verify($credentials['liff_id_token']);
         if (!$user = $this->provider->retrieveByLiffId($liff['sub'])) {
             return false;
         }
-        if ($update) {
-            $user->update(['name' => $liff['name'], 'picture' => $liff['picture']]);
-        }
+        $user->update(['name' => $liff['name'], 'picture' => $liff['picture']]);
 
         return $login ? $this->login($user) : true;
     }
