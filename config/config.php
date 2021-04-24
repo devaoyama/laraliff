635f5f9d3587962a14b231ca59c6a8ac30da4244
devkeita <keita.mywork@gmail.com>
2021-04-25 00:22:45 +0900

    update: add fields config

--- config/config.php
+++ config/config.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-    'liff_channel_id' => env('LIFF_CHANNEL_ID', 'liff_channel_id')
+    'liff_channel_id' => env('LIFF_CHANNEL_ID', 'liff_channel_id'),
+    'fields' => [
+        'liff_id' => 'liff_id',
+        'name' => 'name',
+        'picture' => 'picture',
+    ],
 ];
